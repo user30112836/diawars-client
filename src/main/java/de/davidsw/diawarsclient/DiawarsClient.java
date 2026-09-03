7a832c32831e319553bed9fc13c87c88be2a8d26
David Shayan Wortmann <david.s.wortmann@gmail.com>
2026-09-03 19:36:49 +0200

    Update

--- src/main/java/de/davidsw/diawarsclient/DiawarsClient.java
+++ src/main/java/de/davidsw/diawarsclient/DiawarsClient.java
@@ -2,10 +2,13 @@ package de.davidsw.diawarsclient;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 
 public class DiawarsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		PayloadTypeRegistry.serverboundPlay().register(ClientInfoPayload.TYPE, ClientInfoPayload.CODEC);
+
 		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
 			String json = ClientInfoCollector.collectAsJson();
 			sender.sendPacket(new ClientInfoPayload(json));
